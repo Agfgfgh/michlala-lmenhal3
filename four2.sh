d93d1099bfae3ac4c92422afef10dac747882876
Agfgfgh <97993381+Agfgfgh@users.noreply.github.com>
2024-03-13 10:44:06 +0200

    Add files via upload
    סקרפיט פיבונצי מקוצר שמחפש אם נימצא בסידרה ומדפיס את המיקום שלו

---
+++ four2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+is_fibonacci() {
+    num=$1
+    a=0
+    b=1
+    pos=1
+
+    while [ $b -le $num ]; do
+        if [ $b -eq $num ]; then
+            echo "$num is part of the Fibonacci sequence at position $pos."
+            return 0
+        fi
+        next=$((a + b))
+        a=$b
+        b=$next
+        ((pos++))
+    done
+    echo "$num is not part of the Fibonacci sequence."
+    return 1
+}
+
+read -p "Enter a number: " number
+is_fibonacci "$number"
+
